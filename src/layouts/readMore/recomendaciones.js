050b68e44899cb97f0bb525091dcb0e897078219
Juan Pablo Pantoja <juan.pablo.pantoja@correounivalle.edu.co>
2023-12-01 15:27:47 -0500

    Avance read more

---
+++ src/layouts/readMore/recomendaciones.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recomendaciones = () => {
+    return (
+      <>
+        Hola, este es un componente de React.
+        Este componente muestra un texto simple utilizando React.
+      </>
+    );
+  };
+
+export default Recomendaciones;
